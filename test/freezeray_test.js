e49d812cfc1070a161c526486470c622c51d44f2
Chris Jaure <chrisjaure@gmail.com>
2012-08-23 22:02:00 -0500

    add more useful tests

--- test/freezeray_test.js
+++ test/freezeray_test.js
@@ -1,8 +1,8 @@
 var
 	path = require('path'),
 	fs = require('fs'),
+	assert = require('assert'),
 	wrench = require('wrench'),
-	chai = require('chai'),
 	freezeray = require('../'),
 	app = require('./fixture/app'),
 	config = {
@@ -18,6 +18,24 @@ describe('freezeray', function() {
 		freezeray(app, config).on('done', function() {
 			done();
 		});
+
+	});
+
+	it('should copy the public directory', function() {
+
+		assert(fs.existsSync(path.join(config.publishDir, 'test.css')));
+
+	});
+
+	it('should scrape the index page', function() {
+
+		assert(fs.existsSync(path.join(config.publishDir, 'index.html')));
+
+	});
+
+	it('should create a folder and an index page if the path doesn\'t include an extension', function() {
+
+		assert(fs.existsSync(path.join(config.publishDir, 'test', 'index.html')));
 
 	});
 
